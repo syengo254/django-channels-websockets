571b3085db9ed2da6170821d07b4bc459c0884e1
David Syengo <d.syengo@institute.global>
2023-11-08 15:08:49 +0300

    wip: make chatconsumer async

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,32 +1,29 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_group_name = 'test'
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'type': 'connection_established',
             'message': 'Connect to websockets server succesfully!',
         }))
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         received_data = json.loads(text_data)
 
         # print('Recieved message of type', received_data['type'])
         # print('Message:', received_data['message'])
 
         # send received message to group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
            self.room_group_name,
            {
                'type': 'chat_message',
@@ -35,18 +32,18 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # read message from group (received method did this) & send to clients
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'type': 'chat',
             'message': message,
         }))
 
-    def disconnect(self, code):
+    async def disconnect(self, code):
         # leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    def close(self, code=None):
-        return super().close(code)
+    async def close(self, code=None):
+        return await super().close(code)
