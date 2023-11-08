ff1bbc2065558720861d1291e3b8afe0c696b583
David Syengo <d.syengo@institute.global>
2023-11-08 12:08:10 +0300

    wip: renamed app to chat

--- chat/apps.py
+++ chat/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExamplechannelsConfig(AppConfig):
+class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'examplechannels'
+    name = 'chat'
